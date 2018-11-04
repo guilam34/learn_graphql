a8136d4a61f17ae44ae70cc8a7a6803602a671c9
guilam34 <guillaumelam34@gmail.com>
2018-11-04 13:53:53 -0800

    Add song creation and mutations

--- pt2/client/index.js
+++ pt2/client/index.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 
+import App from './components/App';
 import SongList from './components/SongList';
+import SongCreate from './components/SongCreate';
 
 const client = new ApolloClient({});
 
@@ -11,7 +14,12 @@ const Root = () => {
 	return (
 		// Assumes graphQL client is on /graphql
 		<ApolloProvider client={client}>
-			<SongList/>
+			<Router history={hashHistory}>
+				<Route path="/" component={App}>
+					<IndexRoute component={SongList}/>
+					<Route path="/songs/new" component={SongCreate}/>
+				</Route>
+			</Router>
 		</ApolloProvider>
 	);
 };
